e3ab98d4902e456c8faa63d11b84ba086b596f83
Nikhil Sharma <nikhilsharma3492@gmail.com>
2026-07-02 00:37:15 +0530

    Remove ShopWave files, keep portfolio only

--- src/main/java/com/shopwave/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopwave.repository;
-
-import com.shopwave.model.CustomerOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
-}
